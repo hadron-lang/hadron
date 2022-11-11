882d0869c75874253c2ea0946c6bf09dc3252179
MS-AZ <ms.is.coding@gmail.com>
2022-11-11 11:51:51 +0100

    removed array init and resize logging

--- src/types.c
+++ src/types.c
@@ -5,14 +5,12 @@ TArray *initTArray(int size) {
 	array->array = malloc(sizeof(Token *) * size);
 	array->length = 0;
 	array->size = size;
-	printf("init at %i\n", size);
 	return array;
 }
 
 void pushToken(TArray *ptr, Token *token) {
 	if (ptr->size == ptr->length) {
 		ptr->array = realloc(ptr->array, sizeof(Token *) * (ptr->size*=1.5));
-		printf("resize to %i\n", ptr->size);
 	}
 	ptr->array[ptr->length++] = token;
 }
